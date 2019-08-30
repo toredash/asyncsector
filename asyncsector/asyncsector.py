99d2c828698ee7b19b235cf6c2fc006d78708e6d
Tore <tore.lonoy@gmail.com>
2019-08-30 19:36:40 +0200

    API version bump
    Sector has changed their API version: https://minside.sectoralarm.no/Views/AngularTemplates/Templates/_header.html?v1_1_70

--- asyncsector/asyncsector.py
+++ asyncsector/asyncsector.py
@@ -13,7 +13,7 @@ class AsyncSector(object):
     Temperatures = 'Panel/GetTempratures/'
     History = 'Panel/GetPanelHistory/{}'
     Arm = 'Panel/ArmPanel'
-    Version = 'v1_1_68'
+    Version = 'v1_1_70'
 
     @classmethod
     async def create(cls, session, alarm_id, username, password, version=None):
